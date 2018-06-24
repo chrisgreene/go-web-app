a5d4cc59d8a40690d276c02118af56a40bcc5bdb
chrisgreene <chrisgreene@gmail.com>
2018-06-24 11:45:50 -0600

    Initial import

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "net/http"
+)
+
+func main() {
+        http.HandleFunc("/", homeHandler)
+        log.Fatal(http.ListenAndServe(":8181", nil))
+}
+
+func homeHandler(w http.ResponseWriter, request *http.Request) {
+        fmt.Fprintln(w, "home")
+}
